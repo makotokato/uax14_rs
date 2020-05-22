68093488a205ce09f779392dabd10252ef9aa94e
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2020-05-22 14:06:37 +0900

    Add more tests (not completed yet).

---
+++ tests/spec_test.rs
@@ -0,0 +1,100 @@
+extern crate uax14_rs;
+
+use std::char;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+use std::u32;
+use uax14_rs::LineBreakIterator;
+
+#[test]
+fn run_line_break_test() {
+    let failed = [
+        "\u{0023}\u{0308}\u{2014}",
+        "\u{0023}\u{0308}\u{00B4}",
+        "\u{0023}\u{0308}\u{FFFC}",
+        "\u{0023}\u{0308}\u{AC00}",
+        "\u{0023}\u{0308}\u{AC01}",
+        "\u{0023}\u{0308}\u{231A}",
+        "\u{0023}\u{0308}\u{1100}",
+        "\u{0023}\u{0308}\u{11A8}",
+        "\u{0023}\u{0308}\u{1160}",
+        "\u{0023}\u{2329}",
+        "\u{0023}\u{0308}\u{2329}",
+        "\u{0023}\u{0308}\u{1F1E6}",
+        "\u{0023}\u{0308}\u{261D}",
+        "\u{0023}\u{0308}\u{1F3FB}",
+        "\u{0023}\u{00A7}",
+        "\u{0023}\u{50005}",
+        "\u{0023}\u{0E01}",
+        "\u{0023}\u{3041}",
+        "\u{2014}\u{0308}\u{0023}",
+        "\u{2014}\u{0308}\u{0020}\u{2014}",
+        "\u{2014}\u{0308}\u{00B4}",
+        "\u{2014}\u{0308}\u{FFFC}",
+        "\u{2014}\u{0308}\u{AC00}",
+        "\u{2014}\u{0308}\u{AC01}",
+        "\u{2014}\u{0308}\u{05D0}",
+        "\u{2014}\u{0308}\u{231A}",
+        "\u{2014}\u{0020}\u{00A0}",
+        "\u{2014}\u{0308}\u{1100}",
+        "\u{2014}\u{0308}\u{11A8}",
+        "\u{2014}\u{0308}\u{1160}",
+        "\u{2014}\u{0308}\u{0030}",
+        "\u{2014}\u{0308}\u{2329}",
+        "\u{2014}\u{0308}\u{0025}",
+        "\u{2014}\u{0308}\u{0024}",
+        "\u{2014}\u{0308}\u{1F1E6}",
+        "\u{2014}\u{0308}\u{261D}",
+        "\u{2014}\u{0308}\u{1F3FB}",
+        "\u{2014}\u{0308}\u{0028}",
+        "\u{2014}\u{0020}\u{0001}",
+        "\u{2014}\u{0020}\u{200D}",
+        "\u{2014}\u{0308}\u{00A7}",
+        "\u{2014}\u{0308}\u{50005}",
+        "\u{2014}\u{0308}\u{0E01}",
+        "\u{2014}\u{3041}",
+        "\u{0009}\u{0308}\u{0023}",
+        "\u{0009}\u{0308}\u{2014}",
+    ];
+
+    let f = File::open("tools/LineBreakTest.txt");
+    let f = BufReader::new(f.unwrap());
+    for line in f.lines() {
+        let line = line.unwrap();
+        if line.starts_with("#") {
+            continue;
+        }
+        let mut r = line.split("#");
+        let r = r.next();
+        let v: Vec<_> = r.unwrap().split_ascii_whitespace().collect();
+        let mut b: Vec<_> = Vec::new();
+        let mut c: Vec<_> = Vec::new();
+        let mut count = 0;
+        let mut char_len = 0;
+        loop {
+            if count >= v.len() {
+                break;
+            }
+            if count % 2 == 1 {
+                let ch = char::from_u32(u32::from_str_radix(v[count], 16).unwrap()).unwrap();
+                c.push(ch);
+                char_len = char_len + ch.len_utf8();
+            } else {
+                if v[count] == "\u{00d7}" {
+                } else {
+                    assert_eq!(v[count], "\u{00f7}");
+                    b.push(char_len);
+                }
+            }
+            count = count + 1
+        }
+        let s: String = c.into_iter().collect();
+        let mut iter = LineBreakIterator::new(&s);
+        if failed.contains(&&s.as_str()) {
+            assert_ne!(iter.next(), Some(b[0]), "{}", line);
+            continue;
+        }
+        assert_eq!(iter.next(), Some(b[0]), "{}", line);
+    }
+}

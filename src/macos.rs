3ef1d4d68a8c656f54a480a29bcce45441611c61
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2020-10-03 17:52:47 +0900

    Fix build failure again

--- src/macos.rs
+++ src/macos.rs
@@ -72,7 +72,7 @@ pub fn get_next_break_utf16(text: *const u16, length: usize) -> Option<usize> {
 
 pub fn get_line_break_utf16(text: *const u16, length: usize) -> Option<Vec<usize>> {
     if let Some(b) = get_next_break_utf16(text, length) {
-        let breaks = Vec::new();
+        let mut breaks = Vec::new();
         breaks.push(b);
         return Some(breaks);
     }

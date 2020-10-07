a118cc7f1d9bdd7d37781fe51bfda321723da775
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2020-10-07 22:26:35 +0900

    Fix generic breaker.

--- src/generic.rs
+++ src/generic.rs
@@ -1,3 +1,5 @@
-pub fn get_next_break_utf16(_text: *const u16, _length: usize) -> Option<usize> {
+pub fn get_line_break_utf16(_text: *const u16, _length: usize) -> Option<Vec<usize>> {
+    // If https://github.com/w3c/sealreq is defined for line breaker rule,
+    // use it.
     None
 }

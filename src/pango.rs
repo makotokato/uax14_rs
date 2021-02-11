200c9985fbbbe2577e0e275cb988b4d9e495b505
Makoto Kato <m_kato@ga2.so-net.ne.jp>
2021-02-11 17:12:18 +0900

    Clean up

--- src/pango.rs
+++ src/pango.rs
@@ -9,13 +9,17 @@ pub struct PangoLanguage(c_void);
 
 #[repr(C)]
 pub struct PangoLogAttr {
-    // all members are bit field.
+    // all members are bitfields.
     mask: c_uint,
 }
 
 impl PangoLogAttr {
     pub fn is_line_break(&self) -> bool {
-        (self.mask & 1) != 0
+        if cfg!(target_endian = "little") {
+            (self.mask & 1) != 0
+        } else {
+            (self.mask & 0x8000000) != 0
+        }
     }
 }
 
@@ -34,14 +38,14 @@ extern "C" {
 }
 
 pub fn get_line_break_utf16(text: *const u16, length: usize) -> Option<Vec<usize>> {
-    unsafe {
-        let slice = std::slice::from_raw_parts(text, length);
-        let s: String = decode_utf16(slice.iter().cloned())
-            .map(|r| r.unwrap())
-            .collect();
-        let language = pango_language_from_string("en".as_ptr() as *const c_char);
-        let mut attr_buffer = Vec::with_capacity(length + 1);
+    let slice = unsafe { std::slice::from_raw_parts(text, length) };
+    let s: String = decode_utf16(slice.iter().cloned())
+        .map(|r| r.unwrap())
+        .collect();
+    let language = unsafe { pango_language_from_string("en\0".as_ptr() as *const c_char) };
+    let mut attr_buffer = Vec::with_capacity(length + 1);
 
+    unsafe {
         pango_get_log_attrs(
             s.as_ptr() as *const i8,
             s.len() as i32,
@@ -49,25 +53,26 @@ pub fn get_line_break_utf16(text: *const u16, length: usize) -> Option<Vec<usize
             language,
             attr_buffer.as_mut_ptr(),
             (length + 1) as i32,
-        );
+        )
+    };
 
-        let attrs = std::slice::from_raw_parts(attr_buffer.as_ptr(), length);
-        let mut i = 0;
-        let mut breaks: Vec<usize> = Vec::new();
-        loop {
-            if i >= length {
-                break;
-            }
-            if attrs[i].is_line_break() {
-                breaks.push(i);
-            }
-            i += 1;
+    let attrs = unsafe { std::slice::from_raw_parts(attr_buffer.as_ptr(), length) };
+    let mut i = 0;
+    let mut breaks: Vec<usize> = Vec::new();
+    loop {
+        if i >= length {
+            break;
         }
-        if breaks.len() > 0 {
-            return Some(breaks);
+        if attrs[i].is_line_break() {
+            breaks.push(i);
         }
-        None
+        i += 1;
     }
+
+    if breaks.is_empty() {
+        return None;
+    }
+    Some(breaks)
 }
 
 #[cfg(test)]
@@ -79,5 +84,12 @@ mod tests {
         let text: [u16; 5] = [0x42, 0x42, 0x42, 0x20, 0x42];
         let breaks = get_line_break_utf16(text.as_ptr(), text.len());
         assert_eq!(breaks.unwrap(), [4], "space");
+
+        let text: [u16; 14] = [
+            0x0e20, 0x0e32, 0x0e29, 0x0e32, 0x0e44, 0x0e17, 0x0e22, 0x0e20, 0x0e32, 0x0e29, 0x0e32,
+            0x0e44, 0x0e17, 0x0e22,
+        ];
+        let breaks = get_line_break_utf16(text.as_ptr(), text.len());
+        assert_eq!(breaks.unwrap(), [4, 7, 11], "Thai test");
     }
 }

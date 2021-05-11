db4dcc2bbaa167004c30fc996639193f554e6537
Ting-Yu Lin <aethanyc@gmail.com>
2021-05-10 17:43:56 -0700

    Port be47846 back from aethanyc/icu4x's segmenter branch
    The modification is porting from:
    
    "Use experimental segmenter_lstm for Thai"
    https://github.com/aethanyc/icu4x/commit/be47846d1e533b41428e630d2d0f543977162db6

--- tests/css_word_break.rs
+++ tests/css_word_break.rs
@@ -181,7 +181,6 @@ fn wordbreak_keepall() {
 fn wordbreak_normal() {
     {
         let s = "\u{0e20}\u{0e32}\u{0e29}\u{0e32}\u{0e44}\u{0e17}\u{0e22}\u{0e20}\u{0e32}\u{0e29}\u{0e32}\u{0e44}\u{0e17}\u{0e22}";
-        #[cfg(any(target_os = "macos", target_os = "windows", target_os = "linux"))]
         normal(s, vec![12, 21, 33, 42], vec![4, 7, 11, 14]);
     }
 
